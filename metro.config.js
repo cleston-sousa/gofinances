b8e7fc7d34b19a5f906d1bce5e386a71955baf60
Cleston Sousa <cleston.vs@gmail.com>
2022-06-25 23:00:50 -0300

    svg transformation dev dependencie and config

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,20 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = Object.assign({}, getDefaultConfig(__dirname), {
-  resolver: { sourceExts: ['jsx', 'js', 'ts', 'tsx'] }
-});
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer')
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...resolver.sourceExts, 'jsx', 'js', 'ts', 'tsx', 'svg']
+  };
+
+  return config;
+})();
